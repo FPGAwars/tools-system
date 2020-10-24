0aa81ee49e8d2764fa3c9fd3e7b5c7eaf4b88211
Obijuan <obijuan.cube@gmail.com>
2020-10-24 17:59:14 +0200

    compile_lsusb.sh: shellcheck passed

--- scripts/compile_lsusb.sh
+++ scripts/compile_lsusb.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # -- Compile lsusb script
 
 VER=1.0.22
@@ -6,9 +7,10 @@ TAR_LIBUSB=$LIBUSB.tar.bz2
 REL_LIBUSB=https://github.com/libusb/libusb/releases/download/v$VER/$TAR_LIBUSB
 
 # -- Setup
-. $WORK_DIR/scripts/build_setup.sh
+# shellcheck source=build_setup.sh
+. "$WORK_DIR"/scripts/build_setup.sh
 
-cd $UPSTREAM_DIR
+cd "$UPSTREAM_DIR" || exit
 
 # -- Check and download the release
 test -e $TAR_LIBUSB || wget $REL_LIBUSB
@@ -17,25 +19,25 @@ test -e $TAR_LIBUSB || wget $REL_LIBUSB
 tar jxf $TAR_LIBUSB
 
 # -- Copy the upstream sources into the build directory
-rsync -a $LIBUSB $BUILD_DIR --exclude .git
+rsync -a "$LIBUSB" "$BUILD_DIR" --exclude .git
 
-cd $BUILD_DIR/$LIBUSB
+cd "$BUILD_DIR"/"$LIBUSB" || exit
 
 PREFIX=$BUILD_DIR/$LIBUSB/release
 
 #-- Build libusb
-if [ $ARCH != "darwin" ]; then
-  ./configure --prefix=$PREFIX --host=$HOST --enable-udev=no $CONFIG_FLAGS
+if [ "$ARCH" != "darwin" ]; then
+  ./configure --prefix="$PREFIX" --host=$HOST --enable-udev=no "$CONFIG_FLAGS"
   make -j$J
   make install
 fi
 
 #-- Build lsusb
-cd examples
-if [ $ARCH == "darwin" ]; then
+cd examples || exit
+if [ "$ARCH" == "darwin" ]; then
   $CC -o lsusb listdevs.c -lusb-1.0 -I../libusb
 else
-  $CC -o lsusb listdevs.c -static -lusb-1.0 -lpthread -L$PREFIX/lib -I$PREFIX/include/libusb-1.0
+  $CC -o lsusb listdevs.c -static -lusb-1.0 -lpthread -L"$PREFIX"/lib -I"$PREFIX"/include/libusb-1.0
 fi
 cd ..
 
@@ -43,4 +45,4 @@ cd ..
 test_bin examples/lsusb
 
 # -- Copy the executable into the packages/bin dir
-cp examples/lsusb $PACKAGE_DIR/$NAME/bin/lsusb$EXE
+cp examples/lsusb "$PACKAGE_DIR"/"$NAME"/bin/lsusb$EXE
