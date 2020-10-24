a473ca568fbafe4b5f9627bfdd777465edf8a98b
Obijuan <obijuan.cube@gmail.com>
2020-10-24 18:17:05 +0200

    install_dependencies.sh: shellchekc passed

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,43 +1,44 @@
+#!/bin/bash
 # Install dependencies script
 
-if [ $ARCH == "linux_x86_64" ]; then
+if [ "$ARCH" == "linux_x86_64" ]; then
   sudo apt-get install -y build-essential cmake pkg-config
   sudo apt-get autoremove -y
 fi
 
-if [ $ARCH == "linux_i686" ]; then
+if [ "$ARCH" == "linux_i686" ]; then
   sudo apt-get install -y build-essential cmake pkg-config \
                           gcc-multilib g++-multilib
   sudo apt-get autoremove -y
 fi
 
-if [ $ARCH == "linux_armv7l" ]; then
+if [ "$ARCH" == "linux_armv7l" ]; then
   sudo apt-get install -y build-essential cmake pkg-config \
                           gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
   sudo apt-get autoremove -y
 fi
 
-if [ $ARCH == "linux_aarch64" ]; then
+if [ "$ARCH" == "linux_aarch64" ]; then
   sudo apt-get install -y build-essential cmake pkg-config \
                           gcc-aarch64-linux-gnu g++-aarch64-linux-gnu
   sudo apt-get autoremove -y
 fi
 
-if [ ${ARCH:0:7} == "windows" ]; then
+if [ "${ARCH:0:7}" == "windows" ]; then
   sudo apt-get install -y build-essential cmake pkg-config \
                           mingw-w64 mingw-w64-tools
   sudo apt-get autoremove -y
 fi
 
-if [ $ARCH == "darwin" ]; then
-  which -s brew
-  if [[ $? != 0 ]] ; then
+if [ "$ARCH" == "darwin" ]; then
+  if ! command -v brew;
+  then
       # Install Homebrew
       ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   else
       brew update
   fi
   DEPS="pkg-config libusb libftdi wget confuse"
-  brew install --force $DEPS
-  brew unlink $DEPS && brew link --force $DEPS
+  brew install --force "$DEPS"
+  brew unlink "$DEPS" && brew link --force "$DEPS"
 fi
